3f3a7a857e8642994313ce66617d015974b6934e
alirezanaderialireza <alireza.naderi.rarei@gmail.com>
2025-10-10 14:36:30 +0330

    ll

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,6 +1,8 @@
-// These are CORS headers. They allow the browser to talk to your Supabase Edge Function.
-// For more details, see: https://supabase.com/docs/guides/functions/cors
+// supabase/functions/_shared/cors.ts
+
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*", // We use '*' for now to ensure it works, then we can restrict it.
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Origin": "https://aidashirazi.ir",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
 };
+
